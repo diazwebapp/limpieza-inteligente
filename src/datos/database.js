48296aa7df8359d391f0ad0e2c341ef06784f1d9
diaz web app <diazwebapp@gmail.com>
2025-06-07 18:54:12 -0400

    separadas url tipos de suelo

---
+++ src/datos/database.js
@@ -0,0 +1,72 @@
+export const marcas = [
+    {
+        title: "Robots Aspirador Friegasuelos Cecotec | Conga: Limpieza Inteligente",
+        image: "/robot-cecotec.webp",
+        thubnail: "/robot-cecotec.webp",
+        link: "/cecotec",
+        description: "Descubre el robot aspirador y friegasuelos Cecotec Conga perfecto para tu hogar. Aspira y friega a la vez con tecnología inteligente. ¡Adiós a la suciedad, hola a la comodidad!."
+    },
+    {
+        title: "Roomba Aspirador Friegasuelos | iRobot Roomba Combo: Limpieza Definitiva",
+        image: "/robot-roomba.webp",
+        thubnail: "/robot-roomba.webp",
+        link: "/roomba",
+        description: "Descubre el Roomba aspirador fregasuelos ideal para tu hogar. La tecnología iRobot Roomba Combo aspira y friega a la vez. ¡Máxima eficiencia y limpieza sin esfuerzo!"
+    },
+    {
+        title: "Robot Aspirador Friegasuelos Xiaomi | Mi Robot Vacuum: Limpieza Inteligente",
+        image: "/robot-xiaomi.webp",
+        thubnail: "/robot-xiaomi.webp",
+        link: "/xiaomi",
+        description: "Descubre el robot aspirador y fregasuelos Xiaomi ideal para tu hogar. La serie Mi Robot Vacuum aspira y friega a la vez. ¡Tecnología y eficiencia al mejor precio!"
+    }
+    // Puedes añadir más categorías aquí
+]
+
+export const tiposSuelo =[
+    {
+        title: "Para suelos laminado",
+        image: "/robot-parquet.webp",
+        thubnail: "/robot-parquet.webp",
+        link: "/guia-pisos#laminado",
+        description: ""
+    },
+    {
+        title: "Para pisos de madera",
+        image: "/robot-baldosas.webp",
+        thubnail: "/robot-baldosas.webp",
+        link: "/guia-pisos#madera",
+        description: ""
+    },
+    {
+        title: "Para marmol y piedra",
+        image: "/robot-alfombra.webp",
+        thubnail: "/robot-alfombra.webp",
+        link: "/guia-pisos#marmol",
+        description: ""
+    }
+]
+
+export const amazonProduts = [
+    {
+       title:"iRobot Roomba Combo Essential, Robot Aspirador y friegasuelos",
+    urlAfiliado:"https://amzn.to/3FBUnWJ",
+    imageUrl:"https://m.media-amazon.com/images/I/81y+6dYOxTL._AC_SL1500_.jpg",
+    stars:3.9,
+    ratingCount: 11669,
+    },
+    {
+       title:"Ultenic T10 Lite Robot Aspirador con Base Autovaciado 18000Pa, 60Días Manos Libres, Limpieza Matricial, Auto-Ajuste Potencia para Alfombra, Robot Aspirador y Fregasuelos con LIDAR para Mascota Niños",
+    urlAfiliado:"https://amzn.to/3TeNMEE",
+    imageUrl:"https://m.media-amazon.com/images/I/81-6nim6TML._AC_SL1500_.jpg",
+    stars:4.4,
+    ratingCount: 902,
+    },
+{
+       title:"DREAME L10s Ultra Gen 2 Robot Aspirador y Fregasuelos con RoboSwing MopExtend, 10000 Pa, Modo para Mascotas, Estación Base Automática, 32 Ajustes de Nivel de Agua, Limpieza de Alfombras Versátil",
+    urlAfiliado:"https://amzn.to/43Mb5dW",
+    imageUrl:"https://m.media-amazon.com/images/I/61ijFwUV-wL._AC_SL1500_.jpg",
+    stars:4.3,
+    ratingCount: 1899,
+    }
+]

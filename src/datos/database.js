5eda9cdabb0574970d5b2426d84f7c49c8616770
diaz web app <diazwebapp@gmail.com>
2025-06-09 13:50:07 -0400

    añadida nueva pagins irobot y bravaa

--- src/datos/database.js
+++ src/datos/database.js
@@ -1,5 +1,6 @@
 export const marcas = [
     {
+        name: "cecotec",
         title: "Robots Aspirador Friegasuelos Cecotec | Conga: Limpieza Inteligente",
         image: "/robot-cecotec.webp",
         thubnail: "/robot-cecotec.webp",
@@ -7,13 +8,33 @@ export const marcas = [
         description: "Conga de Cecotec: aspira y friega con tecnología avanzada. Limpieza fácil y eficiente para tu hogar."
     },
     {
-        title: "Roomba Aspirador Friegasuelos | iRobot Roomba Combo: Limpieza Definitiva",
+        name: "irobot",
+        title: "iRobot: Dominando la Limpieza del Hogar",
         image: "/robot-roomba.webp",
         thubnail: "/robot-roomba.webp",
-        link: "/roomba",
-        description: "Roomba Combo: aspira y friega en una sola pasada. Ideal para quienes buscan comodidad y resultados."
+        link: "/irobot",
+        description: "iRobot: la solución definitiva para aspirar y fregar. Limpieza autónoma, eficiente y profunda con la gama Roomba y más.",
+        subitems: [
+            {
+                name: "roomba",
+                title: "Roomba Aspirador Friegasuelos | iRobot Roomba Combo: Limpieza Definitiva",
+                image: "/robot-roomba.webp",
+                thubnail: "/robot-roomba.webp",
+                link: "/irobot/roomba",
+                description: "Roomba Combo: aspira y friega en una sola pasada. Ideal para quienes buscan comodidad y resultados."
+            },
+            {
+                name: "bravaa",
+                title: "Domina la Limpieza con iRobot Braava: La Revolución en Fregado Inteligente",
+                image: "/robot-roomba.webp",
+                thubnail: "/robot-roomba.webp",
+                link: "/irobot/bravaa",
+                description: "Descubre la eficiencia y tecnología avanzada de iRobot Braava para un fregado inteligente y resultados profesionales en tu hogar."
+            }
+        ]
     },
     {
+        name: "xiaomi",
         title: "Robot Aspirador Friegasuelos Xiaomi | Mi Robot Vacuum: Limpieza Inteligente",
         image: "/robot-xiaomi.webp",
         thubnail: "/robot-xiaomi.webp",
@@ -25,6 +46,7 @@ export const marcas = [
 
 export const tiposSuelo =[
     {
+        name: "suelo-laminado",
         title: "Para suelos laminado",
         image: "/para-laminado.webp",
         thubnail: "/para-laminado.webp",
@@ -32,6 +54,7 @@ export const tiposSuelo =[
         description: "El suelo laminado requiere limpieza delicada y control de humedad para evitar daños y mantener su brillo."
     },
     {
+        name: "suelo-madera",
         title: "Para pisos de madera",
         image: "/para-madera.webp",
         thubnail: "/para-madera.webp",
@@ -39,6 +62,7 @@ export const tiposSuelo =[
         description: "La madera necesita cuidado especial: limpieza suave, mínima humedad y protección contra arañazos."
     },
     {
+        name: "suelo-marmol",
         title: "Para marmol y piedra",
         image: "/para-marmol.png",
         thubnail: "/para-marmol.png",
@@ -46,6 +70,7 @@ export const tiposSuelo =[
         description: "El mármol y la piedra requieren limpieza frecuente, productos neutros y evitar exceso de agua para conservar su elegancia."
     },
     {
+        name: "para-alfombras",
         title: "Para Alfombras",
         image: "/para-alfombras.webp",
         thubnail: "/para-alfombras.webp",

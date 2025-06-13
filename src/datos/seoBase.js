1d938749868dbb596442a56cfc358f43dddd81e5
diaz web app <diazwebapp@gmail.com>
2025-06-12 20:15:15 -0400

    json+jd

--- src/datos/seoBase.js
+++ src/datos/seoBase.js
@@ -1,56 +1,70 @@
-export const seoBase = {
-  website: {
-    "@type": "WebSite",
-    "@id": "https://limpiezainteligente.store/#website",
-    "url": "https://limpiezainteligente.store/",
-    "name": "Limpieza Inteligente",
-    "description": "Guía de robots aspiradores y limpieza inteligente para el hogar.",
-    "inLanguage": "es"
-  },
-  author: {
-    "@type": "Person",
-    "name": "diaz web app",
-    "url": "https://github.com/diazwebapp"
-  },
-  publisher: {
-    "@type": "Person",
-    "name": "diaz web app",
-    "logo": {
-      "@type": "ImageObject",
-      "url": "https://avatars.githubusercontent.com/u/90859089?v=4"
-    }
-  },
-  socialImage: "https://limpiezainteligente.store/og-default.webp"
+// Variables reutilizables globales
+export const AUTHOR = {
+  "@type": "Person",
+  name: "Diaz Web App",
+  url: "https://github.com/diazwebapp"
 };
 
-export function buildSeoArticle({
-  title,
+export const PUBLISHER = {
+  "@type": "Organization",
+  name: "Limpieza Inteligente Store",
+  logo: {
+    "@type": "ImageObject",
+    url: "http://limpiezainteligente.store/favicon.ico"
+  }
+};
+
+export const WEBSITE_URL = "https://limpiezainteligente.store/";
+export const WEBSITE_NAME = "Limpieza Inteligente";
+export const WEBSITE_DESCRIPTION = "Guía de robots aspiradores y limpieza inteligente para el hogar.";
+export const WEBSITE_LANGUAGE = "es";
+
+// Plantilla reutilizable para WebSite
+export const schemaWebSite = ({
+  url = WEBSITE_URL,
+  name = WEBSITE_NAME,
+  description = WEBSITE_DESCRIPTION,
+  inLanguage = WEBSITE_LANGUAGE
+} = {}) => ({
+  "@context": "https://schema.org",
+  "@type": "WebSite",
+  "@id": url + "#website",
+  url,
+  name,
   description,
-  canonicalURL,
-  pubDatetime,
-  modDatetime,
-  author = seoBase.author,
-  socialImageURL = seoBase.socialImage
-}) {
-  return {
-    "@context": "https://schema.org",
-    "@graph": [
-      seoBase.website,
-      {
-        "@type": "BlogPosting",
-        "mainEntityOfPage": {
-          "@type": "WebPage",
-          "@id": canonicalURL
-        },
-        "headline": title,
-        "description": description,
-        "image": socialImageURL,
-        "author": author,
-        "publisher": seoBase.publisher,
-        "datePublished": pubDatetime,
-        "dateModified": modDatetime,
-        "inLanguage": "es"
-      }
-    ]
-  };
-}
+  inLanguage
+});
+
+// Plantilla reutilizable para Article/TechArticle
+export const schemaArticle = ({
+  headline,
+  image = [],
+  datePublished,
+  dateModified,
+  author = AUTHOR,
+  publisher = PUBLISHER,
+  description,
+  articleBody,
+  mainEntityOfPage
+} = {}) => ({
+  "@context": "https://schema.org",
+  "@type": "Article",
+  headline,
+  image,
+  datePublished,
+  dateModified,
+  author,
+  publisher,
+  description,
+  articleBody,
+  mainEntityOfPage
+});
+
+// Plantilla reutilizable para FAQPage
+export const schemaFAQ = ({
+  mainEntity = []
+} = {}) => ({
+  "@context": "https://schema.org",
+  "@type": "FAQPage",
+  mainEntity
+});

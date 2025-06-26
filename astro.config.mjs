a2a0969ed08d779efa3e5ab1b9f6f6340fbe69b4
diaz web app <diazwebapp@gmail.com>
2025-06-26 03:23:32 -0400

    creada alt-site

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+    output: 'static',
     site: process.env.PUBLIC_SITE_URL || 'https://limpiezainteligente.store',
     integrations: [sitemap()]
 });

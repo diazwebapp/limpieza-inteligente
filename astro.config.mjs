77b067e64d1843f961a0e5b98deaf7c1914f2da3
diaz web app <diazwebapp@gmail.com>
2025-07-03 14:39:06 -0400

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, passthroughImageService } from 'astro/config';
 
 import sitemap from '@astrojs/sitemap';
 
@@ -20,5 +20,6 @@ export default defineConfig({
     integrations: [sitemap()],
     image: {
         domains: ['gumtreeau-res.cloudinary.com', 's1.elespanol.com'],
-    }
+        service: passthroughImageService()
+    },
 });
